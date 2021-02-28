e3ab9d222901f187e374e3dc75bb1e7072a18012
mehmet <mechitoua_amine@protonmail.ch>
2021-02-28 14:37:50 +0100

    changing some files in warmup1

--- src/code/codingbat/warmup1/Front22.java
+++ src/code/codingbat/warmup1/Front22.java
@@ -1,14 +1,14 @@
 package code.codingbat.warmup1;
 
-/*
-Given a string, take the first 2 chars and return the string
-with the 2 chars added at both the front and back,
-so "kitten" yields"kikittenki". If the string length is less than 2,
-use whatever chars are there.
-
-front22("kitten") → "kikittenki"
-front22("Ha") → "HaHaHa"
-front22("abc") → "ababcab"
+/**
+ * Given a string, take the first 2 chars and return the string
+ * with the 2 chars added at both the front and back,
+ * so "kitten" yields"kikittenki". If the string length is less than 2,
+ * use whatever chars are there.
+ *
+ * front22("kitten") → "kikittenki"
+ * front22("Ha") → "HaHaHa"
+ * front22("abc") → "ababcab"
  */
 public class Front22 {
     public static void main(String[] args) {
--- src/code/codingbat/warmup1/IcyHot.java
+++ src/code/codingbat/warmup1/IcyHot.java
@@ -1,11 +1,12 @@
 package code.codingbat.warmup1;
 
-/*
-Given two temperatures, return true if one is less than 0 and the other is greater than 100.
-
-icyHot(120, -1) → true
-icyHot(-1, 120) → true
-icyHot(2, 120) → false
+/**
+ * Given two temperatures, return true if one is less than 0
+ * and the other is greater than 100.
+ *
+ * icyHot(120, -1) → true
+ * icyHot(-1, 120) → true
+ * icyHot(2, 120) → false
  */
 public class IcyHot {
     public static void main(String[] args) {
