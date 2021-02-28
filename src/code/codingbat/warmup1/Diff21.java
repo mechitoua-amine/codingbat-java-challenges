445d99e5c643bddbe36d2360c6736019e8c7591d
mehmet <mechitoua_amine@protonmail.ch>
2021-02-28 14:09:03 +0100

    changing files in warmup1

--- src/code/codingbat/warmup1/Diff21.java
+++ src/code/codingbat/warmup1/Diff21.java
@@ -1,20 +1,14 @@
 package code.codingbat.warmup1;
 
-/*
-
-Given an int n, return the absolute difference between n and 21,
-except return double the absolute difference if n is over 21.
-
-diff21(19) → 2
-diff21(10) → 11
-diff21(21) → 0
-diff21(25) → 8
-diff21(22) → 2
-
- */
-
 /**
- * class diff21
+ * Given an int n, return the absolute difference between n and 21,
+ * except return double the absolute difference if n is over 21.
+ *
+ * diff21(19) → 2
+ * diff21(10) → 11
+ * diff21(21) → 0
+ * diff21(25) → 8
+ * diff21(22) → 2
  */
 public class Diff21 {
 
--- src/code/codingbat/warmup1/EndUp.java
+++ src/code/codingbat/warmup1/EndUp.java
@@ -1,14 +1,16 @@
 package code.codingbat.warmup1;
 
-/*
-Given a string, return a new string where the last 3 chars are now in upper case.
-If the string has less than 3 chars, uppercase whatever is there.
-Note that str.toUpperCase() returns the uppercase version of a string.
-
-endUp("Hello") → "HeLLO"
-endUp("hi there") → "hi thERE"
-endUp("hi") → "HI"
+/**
+ * Given a string, return a new string where the last 3 chars
+ * are now in upper case. If the string has less than 3 chars,
+ * uppercase whatever is there.Note that str.toUpperCase() returns
+ * the uppercase version of a string.
+ *
+ * endUp("Hello") → "HeLLO"
+ * endUp("hi there") → "hi thERE"
+ * endUp("hi") → "HI"
  */
+
 public class EndUp {
     public static void main(String[] args) {
         System.out.println(endUp("Hello"));
@@ -29,7 +31,7 @@ public class EndUp {
      * if (str.length() <= 3) return str.toUpperCase(); int cut = str.length() - 3;
      * String front = str.substring(0, cut); String back = str.substring(cut); //
      * this takes from cut to the end
-     * 
+     *
      * return front + back.toUpperCase()
      */
 }
