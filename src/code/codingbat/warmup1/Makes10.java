408d781fb96a9fb2addd2e56ca9e5253529f7842
mehmet <mechitoua_amine@protonmail.ch>
2021-03-01 20:34:47 +0100

    changing Makes10.java

--- src/code/codingbat/warmup1/Makes10.java
+++ src/code/codingbat/warmup1/Makes10.java
@@ -1,12 +1,13 @@
 package code.codingbat.warmup1;
 
-/*
-Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+/**
+ * Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
 
-makes10(9, 10) → true
-makes10(9, 9) → false
-makes10(1, 9) → true
+ * makes10(9, 10) → true
+ * makes10(9, 9) → false
+ * makes10(1, 9) → true
  */
+
 public class Makes10 {
 
     public static void main(String[] args) {
