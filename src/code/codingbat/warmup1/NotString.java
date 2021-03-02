623ef116b644687d1d09281383fb87333a810a65
mehmet <mechitoua_amine@protonmail.ch>
2021-03-02 21:20:57 +0100

    changing NotString.java

--- src/code/codingbat/warmup1/NotString.java
+++ src/code/codingbat/warmup1/NotString.java
@@ -1,21 +1,16 @@
 package code.codingbat.warmup1;
 
-/*
-Given a string, return a new string where "not " has been added to the front. However,
-if the string already begins with "not", return the string unchanged.
-Note: use .equals() to compare 2 strings.
-
-notString("candy") → "not candy"
-notString("x") → "not x"
-notString("not bad") → "not bad"
- */
-
 /**
- * Class NotString
+ * Given a string, return a new string where "not " has been added to the front.
+ * However,if the string already begins with "not", return the string unchanged.
+ * Note: use .equals() to compare 2 strings.
+
+ * notString("candy") → "not candy"
+ * notString("x") → "not x"
+ * notString("not bad") → "not bad"
  */
 
 public class NotString {
-
     public static void main(String[] args) {
         System.out.println(notString("candy"));
         System.out.println(notString("x"));
