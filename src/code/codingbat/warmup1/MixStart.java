6fced4358866af6637d5c29f680f3334b9fbfd18
mehmet <mechitoua_amine@protonmail.ch>
2021-03-02 21:18:27 +0100

    changing MixStart.java

--- src/code/codingbat/warmup1/MixStart.java
+++ src/code/codingbat/warmup1/MixStart.java
@@ -1,12 +1,12 @@
 package code.codingbat.warmup1;
 
-/*
-Return true if the given string begins with "mix", except the 'm' can be anything,
-so "pix", "9ix" .. all count.
+/**
+ * Return true if the given string begins with "mix", except the 'm' can be anything,
+ * so "pix", "9ix" .. all count.
 
-mixStart("mix snacks") → true
-mixStart("pix snacks") → true
-mixStart("piz snacks") → false
+ * mixStart("mix snacks") → true
+ * mixStart("pix snacks") → true
+ * mixStart("piz snacks") → false
  */
 public class MixStart {
     public static void main(String[] args) {
