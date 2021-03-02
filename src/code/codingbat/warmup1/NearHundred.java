68b010fc0dcc872a3d665d289ca8eaf04eb293ef
mehmet <mechitoua_amine@protonmail.ch>
2021-03-02 21:20:07 +0100

    changing NearHundred.java

--- src/code/codingbat/warmup1/NearHundred.java
+++ src/code/codingbat/warmup1/NearHundred.java
@@ -1,15 +1,15 @@
 package code.codingbat.warmup1;
-/*
-Given an int n, return true if it is within 10 of 100 or 200.
-Note: Math.abs(num) computes the absolute value of a number.
 
-nearHundred(93) → true
-nearHundred(90) → true
-nearHundred(89) → false
+/**
+ * Given an int n, return true if it is within 10 of 100 or 200.
+ * Note: Math.abs(num) computes the absolute value of a number.
+
+ * nearHundred(93) → true
+ * nearHundred(90) → true
+ * nearHundred(89) → false
  */
 
 public class NearHundred {
-
     public static void main(String[] args) {
         System.out.println(nearHundred(93));
         System.out.println(nearHundred(90));
