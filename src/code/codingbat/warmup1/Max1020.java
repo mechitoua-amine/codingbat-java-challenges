0d2f69327c6a1a4a53d9db1dac8eb06e9a096277
mehmet <mechitoua_amine@protonmail.ch>
2021-03-02 21:17:35 +0100

    changing Max1020.java && MissingChar.java

--- src/code/codingbat/warmup1/Max1020.java
+++ src/code/codingbat/warmup1/Max1020.java
@@ -1,13 +1,14 @@
 package code.codingbat.warmup1;
 
-/*
-Given 2 positive int values, return the larger value that is in the range 10..20 inclusive,
-or return 0 if neither is in that range.
+/**
+ * Given 2 positive int values, return the larger value that is in the range 10..20 inclusive,
+ * or return 0 if neither is in that range.
 
-max1020(11, 19) → 19
-max1020(19, 11) → 19
-max1020(11, 9) → 11
+ * max1020(11, 19) → 19
+ * max1020(19, 11) → 19
+ * max1020(11, 9) → 11
  */
+
 public class Max1020 {
     public static void main(String[] args) {
         System.out.println(max1020(11, 19));
@@ -33,7 +34,7 @@ public class Max1020 {
     /*
      * // First make it so the bigger value is in a if (b > a) { int temp = a; a =
      * b; b = temp; }
-     * 
+     *
      * // Knowing a is bigger, just check a first if (a >= 10 && a <= 20) return a;
      * if (b >= 10 && b <= 20) return b; return 0;
      */
--- src/code/codingbat/warmup1/MissingChar.java
+++ src/code/codingbat/warmup1/MissingChar.java
@@ -1,14 +1,15 @@
 package code.codingbat.warmup1;
 
-/*
-Given a non-empty string and an int n, return a new string where the char at index n has been removed.
-The value of n will be a valid index of a char in the original string
-(i.e. n will be in the range 0..str.length()-1 inclusive)
+/**
+ * Given a non-empty string and an int n, return a new string where the char
+ * at index n has been removed.The value of n will be a valid index of a char
+ * in the original string (i.e. n will be in the range 0..str.length()-1 inclusive)
 
-missingChar("kitten", 1) → "ktten"
-missingChar("kitten", 0) → "itten"
-missingChar("kitten", 4) → "kittn"
+ * missingChar("kitten", 1) → "ktten"
+ * missingChar("kitten", 0) → "itten"
+ * missingChar("kitten", 4) → "kittn"
  */
+
 public class MissingChar {
 
     public static void main(String[] args) {
