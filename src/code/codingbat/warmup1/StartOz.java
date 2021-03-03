f5aa45a2ca6b9897d2e77c76ee3e147eb5e3e011
mehmet <mechitoua_amine@protonmail.ch>
2021-03-03 10:44:48 +0100

    changin StartOz.java

--- src/code/codingbat/warmup1/StartOz.java
+++ src/code/codingbat/warmup1/StartOz.java
@@ -1,16 +1,16 @@
 package code.codingbat.warmup1;
 
-/*
-Given a string, return a string made of the first 2 chars (if present),
-however include first char only if it is 'o' and include the second
-only if it is 'z', so "ozymandias" yields "oz".
+/**
+ * Given a string, return a string made of the first 2 chars (if present),
+ * however include first char only if it is 'o' and include the second
+ * only if it is 'z', so "ozymandias" yields "oz".
 
-startOz("ozymandias") → "oz"
-startOz("bzoo") → "z"
-startOz("oxx") → "o"
+ * startOz("ozymandias") → "oz"
+ * startOz("bzoo") → "z"
+ * startOz("oxx") → "o"
  */
-public class StartOz {
 
+public class StartOz {
     public static void main(String[] args) {
         System.out.println(startOz("ozymandias"));
         System.out.println(startOz("bzoo"));
