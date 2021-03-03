0274793aacb6d1817400c0f512a3fab99b370f33
mehmet <mechitoua_amine@protonmail.ch>
2021-03-03 10:42:07 +0100

    changin PosNeg.java

--- src/code/codingbat/warmup1/PosNeg.java
+++ src/code/codingbat/warmup1/PosNeg.java
@@ -1,11 +1,12 @@
 package code.codingbat.warmup1;
-/*
-Given 2 int values, return true if one is negative and one is positive.
-Except if the parameter "negative" is true, then return true only if both are negative.
+/**
+ * Given 2 int values, return true if one is negative and one is positive.
+ * Except if the parameter "negative" is true, then return true only if
+ * both are negative.
 
-posNeg(1, -1, false) → true
-posNeg(-1, 1, false) → true
-posNeg(-4, -5, true) → true
+ * posNeg(1, -1, false) → true
+ * posNeg(-1, 1, false) → true
+ * posNeg(-4, -5, true) → true
  */
 
 public class PosNeg {
