5dd75fdb02d9e6ea48ae6be06fd623275527a048
mehmet <mechitoua_amine@protonmail.ch>
2021-03-03 10:43:04 +0100

    changing Sleeping.java

--- src/code/codingbat/warmup1/Sleepin.java
+++ src/code/codingbat/warmup1/Sleepin.java
@@ -1,13 +1,15 @@
 package code.codingbat.warmup1;
 
-/*
-The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
-We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+/**
+ * The parameter weekday is true if it is a weekday, and the parameter
+ * vacation is true if we are on vacation.We sleep in if it is not a weekday
+ * or we're on vacation. Return true if we sleep in.
 
-sleepIn(false, false) → true
-sleepIn(true, false) → false
-sleepIn(false, true) → true
+ * sleepIn(false, false) → true
+ * sleepIn(true, false) → false
+ * sleepIn(false, true) → true
  */
+
 public class Sleepin {
     public static void main(String[] args) {
         if (sleepIn(false, false)) {
