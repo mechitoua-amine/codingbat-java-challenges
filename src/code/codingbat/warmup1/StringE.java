ce721cded2faee53236d5d59b92e7a094cc990ce
mehmet <mechitoua_amine@protonmail.ch>
2021-03-04 19:02:12 +0100

    changing StringE.java

--- src/code/codingbat/warmup1/StringE.java
+++ src/code/codingbat/warmup1/StringE.java
@@ -1,12 +1,13 @@
 package code.codingbat.warmup1;
 
-/*
-Return true if the given string contains between 1 and 3 'e' chars.
+/**
+ * Return true if the given string contains between 1 and 3 'e' chars.
 
-stringE("Hello") → true
-stringE("Heelle") → true
-stringE("Heelele") → false
+ * stringE("Hello") → true
+ * stringE("Heelle") → true
+ * stringE("Heelele") → false
  */
+
 public class StringE {
     public static void main(String[] args) {
         System.out.println(stringE("Hello"));
