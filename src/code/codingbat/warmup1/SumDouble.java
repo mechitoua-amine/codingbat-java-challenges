9bb15d932886e21d9bb75fdaf6b3ad02f36d621f
mehmet <mechitoua_amine@protonmail.ch>
2021-03-04 19:03:04 +0100

    changing SumDouble.java

--- src/code/codingbat/warmup1/SumDouble.java
+++ src/code/codingbat/warmup1/SumDouble.java
@@ -1,15 +1,15 @@
 package code.codingbat.warmup1;
 
-/*
-Given two int values, return their sum. Unless the two values are the same,
-then return double their sum.
-
-sumDouble(1, 2) → 3
-sumDouble(3, 2) → 5
-sumDouble(2, 2) → 8
+/**
+ * Given two int values, return their sum. Unless the two values are the same,
+ * then return double their sum.
+ *
+ * sumDouble(1, 2) → 3
+ * sumDouble(3, 2) → 5
+ * sumDouble(2, 2) → 8
  */
-public class SumDouble {
 
+public class SumDouble {
     public static void main(String[] args) {
         System.out.println(sumDouble(1, 2));
         System.out.println(sumDouble(3, 2));
