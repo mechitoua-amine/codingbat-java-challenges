b069fe5616998c4873de71ca773232276f37301b
mehmet <mechitoua_amine@protonmail.ch>
2021-03-04 19:06:53 +0100

    changing ArrayCount9.java

--- src/code/codingbat/warmup2/ArrayCount9.java
+++ src/code/codingbat/warmup2/ArrayCount9.java
@@ -1,15 +1,14 @@
 package code.codingbat.warmup2;
 
-/*
-Given an array of ints, return the number of 9's in the array.
+/**
+ * Given an array of ints, return the number of 9's in the array.
 
-arrayCount9([1, 2, 9]) → 1
-arrayCount9([1, 9, 9]) → 2
-arrayCount9([1, 9, 9, 3, 9]) → 3
+ * arrayCount9([1, 2, 9]) → 1
+ * arrayCount9([1, 9, 9]) → 2
+ * arrayCount9([1, 9, 9, 3, 9]) → 3
  */
 
 public class ArrayCount9 {
-
     public static void main(String[] args) {
         int[] a = { 1, 2, 9 };
         int[] b = { 1, 9, 9 };
