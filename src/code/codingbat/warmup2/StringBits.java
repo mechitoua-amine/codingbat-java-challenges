7881d362ecede093b6c18516ed65c683e21899f2
mehmet <mechitoua_amine@protonmail.ch>
2021-03-05 19:01:22 +0100

    changing StringBits.java

--- src/code/codingbat/warmup2/StringBits.java
+++ src/code/codingbat/warmup2/StringBits.java
@@ -1,11 +1,12 @@
 package code.codingbat.warmup2;
-/*
-Given a string, return a new string made of every other char
-starting with the first, so "Hello" yields "Hlo".
 
-stringBits("Hello") → "Hlo"
-stringBits("Hi") → "H"
-stringBits("Heeololeo") → "Hello"
+/**
+ * Given a string, return a new string made of every other char
+ * starting with the first, so "Hello" yields "Hlo".
+ *
+ * stringBits("Hello") → "Hlo"
+ * stringBits("Hi") → "H"
+ * stringBits("Heeololeo") → "Hello"
 */
 
 class StringBits {
