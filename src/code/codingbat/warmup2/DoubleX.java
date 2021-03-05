abce1dda8e9e86f13af2b19376ed99fd01200312
mehmet <mechitoua_amine@protonmail.ch>
2021-03-05 18:57:14 +0100

    changin DoubleX.java

--- src/code/codingbat/warmup2/DoubleX.java
+++ src/code/codingbat/warmup2/DoubleX.java
@@ -1,12 +1,12 @@
 package code.codingbat.warmup2;
 
-/*
-Given a string, return true if the first instance of "x" in the string
-is immediately followed by another "x".
+/**
+ * Given a string, return true if the first instance of "x" in the string
+ * is immediately followed by another "x".
 
-doubleX("axxbb") → true
-doubleX("axaxax") → false
-doubleX("xxxxx") → true
+ * doubleX("axxbb") → true
+ * doubleX("axaxax") → false
+ * doubleX("xxxxx") → true
  */
 
 public class DoubleX {
