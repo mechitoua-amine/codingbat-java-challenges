36e0ca71fb8b19b98d830292ccaadf0e0753f035
mehmet <mechitoua_amine@protonmail.ch>
2021-03-07 10:13:01 +0100

    changing StringTimes.java

--- src/code/codingbat/warmup2/StringTimes.java
+++ src/code/codingbat/warmup2/StringTimes.java
@@ -1,12 +1,12 @@
 package code.codingbat.warmup2;
 
-/*
-Given a string and a non-negative int n,
-return a larger string that is n copies of the original string.
+/**
+ * Given a string and a non-negative int n,
+ * return a larger string that is n copies of the original string.
 
-stringTimes("Hi", 2) → "HiHi"
-stringTimes("Hi", 3) → "HiHiHi"
-stringTimes("Hi", 1) → "Hi"
+ * stringTimes("Hi", 2) → "HiHi"
+ * stringTimes("Hi", 3) → "HiHiHi"
+ * stringTimes("Hi", 1) → "Hi"
  */
 
 public class StringTimes {
