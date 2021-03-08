d3095988055dd1b36488b304ac458cd45b63c985
mehmet <mechitoua_amine@protonmail.ch>
2021-03-08 18:09:51 +0100

    changing the CountClumps.java solution

--- src/code/codingbat/array3/CountClumps.java
+++ src/code/codingbat/array3/CountClumps.java
@@ -18,12 +18,12 @@ class CountClumps {
 
     public static int countClumps(int... nums) {
         int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == nums[i+1])
-                count++;
-                for(int j = i+1; j < nums.length; j++)
-                    if (nums[i] == nums[i+1]) i++;
-                    else break;
+        for (int i = 0; i < nums.length - 1; i++)
+        if (nums[i] == nums[i + 1]) {
+            count++;
+            for (int j = i + 1; j < nums.length; j++)
+                if (nums[j] == nums[i]) i++;
+                else break;
         }
         return count;
     }
