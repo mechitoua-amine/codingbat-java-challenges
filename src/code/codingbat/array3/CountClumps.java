27a744c758e8d516b2c020c08eb8c7d6474b9610
mehmet <mechitoua_amine@protonmail.ch>
2021-03-08 18:06:36 +0100

    adding the CountClumps.java solution

--- src/code/codingbat/array3/CountClumps.java
+++ src/code/codingbat/array3/CountClumps.java
@@ -11,10 +11,20 @@ package code.codingbat.array3;
 
 class CountClumps {
     public static void main(String[] args) {
-        //
+        System.out.println(countClumps(1, 2, 2, 3, 4, 4));
+        System.out.println(countClumps(1, 1, 2, 1, 1));
+        System.out.println(countClumps(1, 1, 1, 1, 1));
     }
 
     public static int countClumps(int... nums) {
-        //
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == nums[i+1])
+                count++;
+                for(int j = i+1; j < nums.length; j++)
+                    if (nums[i] == nums[i+1]) i++;
+                    else break;
+        }
+        return count;
     }
 }
