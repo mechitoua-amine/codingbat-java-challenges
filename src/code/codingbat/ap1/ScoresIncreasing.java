a7ea44f4f4a2aff18e30ab0342cccda86de3cb8f
mehmet <mechitoua_amine@protonmail.ch>
2021-03-08 18:23:45 +0100

    adding the SourceIncreasing.java file

---
+++ src/code/codingbat/ap1/ScoresIncreasing.java
@@ -0,0 +1,22 @@
+package code.codingbat.ap1;
+
+/**
+ * Given an array of scores, return true if each score is equal
+ * or greater than the one before. The array will be length 2 or more.
+ *
+ * scoresIncreasing([1, 3, 4]) → true
+ * scoresIncreasing([1, 3, 2]) → false
+ * scoresIncreasing([1, 1, 4]) → true
+ */
+
+public class ScoresIncreasing {
+    public static void main(String[] args) {
+        System.out.println(scoresIncreasing(1, 3, 4));
+        System.out.println(scoresIncreasing(1, 3, 2));
+        System.out.println(scoresIncreasing(1, 1, 4));
+    }
+
+    public static boolean scoresIncreasing(int... nums) {
+        // code here
+    }
+}
