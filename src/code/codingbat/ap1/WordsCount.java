cdc1d80377aa2e293ed6cfb549551571f525941b
mehmet <mechitoua_amine@protonmail.ch>
2021-03-09 14:27:53 +0100

    adding the WordsCount.java file

---
+++ src/code/codingbat/ap1/WordsCount.java
@@ -0,0 +1,22 @@
+package code.codingbat.ap1;
+
+/**
+ * Given an array of strings, return the count of the number of strings
+ * with the given length.
+ *
+ * wordsCount(["a", "bb", "b", "ccc"], 1) → 2
+ * wordsCount(["a", "bb", "b", "ccc"], 3) → 1
+ * wordsCount(["a", "bb", "b", "ccc"], 4) → 0
+ */
+
+public class WordsCount {
+    public static void main(String[] args) {
+        System.out.println(wordsCount(new String[]{"a", "bb", "b", "ccc"}, 1));
+        System.out.println(wordsCount(new String[]{"a", "bb", "b", "ccc"}, 3));
+        System.out.println(wordsCount(new String[]{"a", "bb", "b", "ccc"}, 4));
+    }
+
+    public static int wordsCount(String[] words, int len) {
+       return 0;
+    }
+}
