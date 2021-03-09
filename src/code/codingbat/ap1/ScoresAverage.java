284f031ad4f418ca71f2bd4f2f5e2812397eac12
mehmet <mechitoua_amine@protonmail.ch>
2021-03-09 14:14:13 +0100

    adding the ScoresAverage.java solution

--- src/code/codingbat/ap1/ScoresAverage.java
+++ src/code/codingbat/ap1/ScoresAverage.java
@@ -24,6 +24,18 @@ public class ScoresAverage {
     }
 
     public static int scoresAverage(int... scores) {
-        return 0;
+        int first = average(scores, 0, scores.length / 2);
+        int second = average(scores, scores.length / 2, scores.length);
+        return Math.max(first, second);
+    }
+
+    public static int average(int[] scores, int start, int end) {
+        int sum = 0;
+        int count = 0;
+        for (int i = start; i < end; i++) {
+            sum += scores[i];
+            count++;
+        }
+        return sum / count;
     }
 }
