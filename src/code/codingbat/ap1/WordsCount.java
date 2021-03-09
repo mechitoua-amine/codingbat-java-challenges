ffe2f886be7bb74ba87619fcd3e350100e996ae2
mehmet <mechitoua_amine@protonmail.ch>
2021-03-09 14:33:53 +0100

    adding the WordsCount.java solution

--- src/code/codingbat/ap1/WordsCount.java
+++ src/code/codingbat/ap1/WordsCount.java
@@ -17,6 +17,11 @@ public class WordsCount {
     }
 
     public static int wordsCount(String[] words, int len) {
-       return 0;
+        int count = 0;
+        for (int i = 0; i < words.length; i++) {
+           if (words[i].length() == len)
+               count++;
+        }
+        return count;
     }
 }
