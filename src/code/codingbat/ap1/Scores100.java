630c951e6b9a0be974c025603a1490b9964794d2
mehmet <mechitoua_amine@protonmail.ch>
2021-03-09 13:34:16 +0100

    adding the Scores100.java solution

--- src/code/codingbat/ap1/Scores100.java
+++ src/code/codingbat/ap1/Scores100.java
@@ -16,6 +16,9 @@ public class Scores100 {
     }
 
     public static boolean scores100(int... scores) {
-        return true;
+        for(int i = 0; i < scores.length-1; i++) {
+            if (scores[i] == 100 && scores[i+1] == 100) return true;
+        }
+        return false;
     }
 }
