8e3607581c6e878dabf7e2934a8a81950bd35563
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-03-10 16:35:21 +0100

    adding the HasOne.java file

---
+++ src/code/codingbat/ap1/HasOne.java
@@ -0,0 +1,22 @@
+package code.codingbat.ap1;
+
+/**
+ * Given a positive int n, return true if it contains a 1 digit.
+ * Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+
+ * hasOne(10) → true
+ * hasOne(22) → false
+ * hasOne(220) → false
+ */
+
+class HasOne {
+    public static void main(String[] args) {
+        System.out.println(hasOne(10));
+        System.out.println(hasOne(22));
+        System.out.println(hasOne(220));
+    }
+
+    public static boolean hasOne(int n) {
+        // implementation
+    }
+}
