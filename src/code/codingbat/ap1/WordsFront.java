8b6a0edad38887714698c5a215282d97db3ef300
mechitoua mohammed amine <mechitoua.m.amine@gmail.com>
2021-03-10 13:35:07 +0100

    adding WordsFront.java file

---
+++ src/code/codingbat/ap1/WordsFront.java
@@ -0,0 +1,22 @@
+package code.codingbat.ap1;
+
+/**
+ * Given an array of strings, return a new array containing the first N strings.
+ * N will be in the range 1..length.
+ *
+ * wordsFront(["a", "b", "c", "d"], 1) → ["a"]
+ * wordsFront(["a", "b", "c", "d"], 2) → ["a", "b"]
+ * wordsFront(["a", "b", "c", "d"], 3) → ["a", "b", "c"]
+ */
+
+public class WordsFront {
+    public static void main(String[] args) {
+        System.out.println(wordsFront(new String[]{"a", "b", "c", "d"}, 1));
+        System.out.println(wordsFront(new String[]{"a", "b", "c", "d"}, 2));
+        System.out.println(wordsFront(new String[]{"a", "b", "c", "d"}, 3));
+    }
+
+    public static String[] wordsFront(String[] words, int n) {
+        return "";
+    }
+}
