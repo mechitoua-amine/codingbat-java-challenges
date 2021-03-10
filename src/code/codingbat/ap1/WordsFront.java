e5ab1cd790ab26e005995d85e32c9e97ae375229
mechitoua mohammed amine <mechitoua.m.amine@gmail.com>
2021-03-10 14:20:01 +0100

    adding WordsFront.java solution

--- src/code/codingbat/ap1/WordsFront.java
+++ src/code/codingbat/ap1/WordsFront.java
@@ -1,5 +1,7 @@
 package code.codingbat.ap1;
 
+import java.util.Arrays;
+
 /**
  * Given an array of strings, return a new array containing the first N strings.
  * N will be in the range 1..length.
@@ -11,12 +13,16 @@ package code.codingbat.ap1;
 
 public class WordsFront {
     public static void main(String[] args) {
-        System.out.println(wordsFront(new String[]{"a", "b", "c", "d"}, 1));
-        System.out.println(wordsFront(new String[]{"a", "b", "c", "d"}, 2));
-        System.out.println(wordsFront(new String[]{"a", "b", "c", "d"}, 3));
+        System.out.println(Arrays.toString(wordsFront(new String[]{"a", "b", "c", "d"}, 1)));
+        System.out.println(Arrays.toString(wordsFront(new String[]{"a", "b", "c", "d"}, 2)));
+        System.out.println(Arrays.toString(wordsFront(new String[]{"a", "b", "c", "d"}, 3)));
+
     }
 
     public static String[] wordsFront(String[] words, int n) {
-        return "";
+        String[] newWords = new String[n];
+        for (int i = 0; i < n; i++)
+            newWords[i] = words[i];
+        return newWords;
     }
 }
