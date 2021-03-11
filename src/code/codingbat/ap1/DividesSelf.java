d5d4c15f04fa9a3578a3a4fdda6fd3450fc8db91
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-03-11 15:38:22 +0100

    adding DividesSelf.java solution

--- src/code/codingbat/ap1/DividesSelf.java
+++ src/code/codingbat/ap1/DividesSelf.java
@@ -20,6 +20,13 @@ public class DividesSelf {
     }
 
     public static boolean dividesSelf(int n) {
-       return true;
+        int copyN = n;
+        while(n > 0) {
+           if (n % 10 == 0) return false;
+           else
+               if (copyN % (n % 10) == 0) n /= 10;
+               else return false;
+        }
+        return true;
     }
 }
