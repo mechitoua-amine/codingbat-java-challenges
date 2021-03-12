0658515895202abefd9cccca2285b1a7bb0b2173
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-03-12 18:14:56 +0100

    adding MatchUp.java file

---
+++ src/code/codingbat/ap1/MatchUp.java
@@ -0,0 +1,33 @@
+package code.codingbat.ap1;
+
+import java.util.Arrays;
+
+/**
+ * Given 2 arrays that are the same length containing strings,
+ * compare the 1st string in one array to the 1st string in the
+ * other array, the 2nd to the 2nd and so on. Count the number
+ * of times that the 2 strings are non-empty and start with the
+ * same char. The strings may be any length, including 0.
+ *
+ * matchUp(["aa", "bb", "cc"], ["aaa", "xx", "bb"]) → 1
+ * matchUp(["aa", "bb", "cc"], ["aaa", "b", "bb"]) → 2
+ * matchUp(["aa", "bb", "cc"], ["", "", "ccc"]) → 1
+ */
+
+public class MatchUp {
+    public static void main(String[] args) {
+        System.out.println(
+                matchUp(new String[]{"aa", "bb", "cc"},
+                        new String[]{"aaa", "xx", "bb"}));
+        System.out.println(
+                matchUp(new String[]{"aa", "bb", "cc"},
+                        new String[]{"aaa", "b", "bb"}));
+        System.out.println(
+                matchUp(new String[]{"aa", "bb", "cc"},
+                        new String[]{"", "", "ccc"}));
+    }
+
+    public static int matchUp(String[] a, String[] b) {
+        return 0;
+    }
+}
