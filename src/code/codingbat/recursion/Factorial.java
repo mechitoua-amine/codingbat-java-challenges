2c33b6e1a76ee3af428fb5486f69aae75f710bf5
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-03-16 15:30:43 +0100

    adding recursion folder && Factorial.java file

---
+++ src/code/codingbat/recursion/Factorial.java
@@ -0,0 +1,21 @@
+package code.codingbat.recursion;
+
+/**
+ * Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1.
+ * Compute the result recursively (without loops).
+ *
+ * factorial(1) → 1
+ * factorial(2) → 2
+ * factorial(3) → 6
+ */
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(factorial(1));
+        System.out.println(factorial(2));
+        System.out.println(factorial(3));
+    }
+
+    public static int factorial(int n) {
+        return 0;
+    }
+}
