8f8fcefe78c5b963bff22215ef8fd7d59b396e36
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-03-16 15:48:28 +0100

    adding Fibonacci.java solution

--- src/code/codingbat/recursion/Fibonacci.java
+++ src/code/codingbat/recursion/Fibonacci.java
@@ -21,6 +21,7 @@ public class Fibonacci {
     }
 
     public static int fibonacci(int n) {
-        return 0;
+        if (n <= 1) return n;
+        return fibonacci(n - 2) + fibonacci(n - 1);
     }
 }
