c295b0675646814a19b40ad2eff300e5ff583dae
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-03-17 15:07:11 +0100

    adding Count7.java solution

--- src/code/codingbat/recursion/Count7.java
+++ src/code/codingbat/recursion/Count7.java
@@ -19,6 +19,8 @@ public class Count7 {
     }
 
     public static int count7(int n) {
-        return 0;
+        if (n == 0) return 0;
+        if (n % 10 == 7) return 1 + count7(n / 10);
+        return count7(n / 10);
     }
 }
