d3e3755b936723c07ac24439a46b69bcf12850fe
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-03-17 15:23:59 +0100

    adding Count8.java solution

--- src/code/codingbat/recursion/Count8.java
+++ src/code/codingbat/recursion/Count8.java
@@ -20,6 +20,9 @@ public class Count8 {
     }
 
     public static int count8(int n) {
-        return 0;
+        if (n == 0) return 0;
+        if (n >= 88 && n % 100 == 88) return 2 + count8(n / 10);
+        if (n % 10 == 8) return 1 + count8(n / 10);
+        return count8(n / 10);
     }
 }
