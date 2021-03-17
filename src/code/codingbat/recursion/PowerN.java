36e36298717eaafc6299891117a3c2e5d65dbf36
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-03-17 15:24:29 +0100

    adding PowerN.java file

--- src/code/codingbat/recursion/PowerN.java
+++ src/code/codingbat/recursion/PowerN.java
@@ -1,1 +1,23 @@
 
+package code.codingbat.recursion;
+
+/**
+ * Given base and n that are both 1 or more, compute recursively (no loops)
+ * the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+ *
+ * powerN(3, 1) → 3
+ * powerN(3, 2) → 9
+ * powerN(3, 3) → 27
+ */
+
+public class PowerN {
+    public static void main(String[] args) {
+        System.out.println(powerN(3, 1));
+        System.out.println(powerN(3, 2));
+        System.out.println(powerN(3, 3));
+    }
+
+    public static int powerN(int base, int n) {
+        return 0;
+    }
+}
