21dc362e56dbee06ec6c6fa712a6ffa05f70f20a
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-03-20 20:54:59 +0100

    adding ChangeXY.java solution

--- src/code/codingbat/recursion/ChangeXY.java
+++ src/code/codingbat/recursion/ChangeXY.java
@@ -17,6 +17,8 @@ class ChangeXY {
     }
 
     public static String changeXY(String str) {
-        return "";
+        if (str.length() == 0) return str;
+        if (str.charAt(0) == 'x') return "y" + changeXY(str.substring(1));
+        return str.charAt(0) + changeXY(str.substring(1));
     }
 }
