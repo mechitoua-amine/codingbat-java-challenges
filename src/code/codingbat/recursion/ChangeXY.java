fc3d384e2c2224168115eb13e4838332afe5f317
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-03-20 20:44:37 +0100

    adding ChangeXY.java file

---
+++ src/code/codingbat/recursion/ChangeXY.java
@@ -0,0 +1,22 @@
+package code.codingbat.recursion;
+
+/**
+ * Given a string, compute recursively (no loops) a new string where
+ * all the lowercase 'x' chars have been changed to 'y' chars.
+ *
+ * changeXY("codex") → "codey"
+ * changeXY("xxhixx") → "yyhiyy"
+ * changeXY("xhixhix") → "yhiyhiy"
+ */
+
+class ChangeXY {
+    public static void main(String[] args) {
+        System.out.println(changeXY("codex"));
+        System.out.println(changeXY("xxhixx"));
+        System.out.println(changeXY("xhixhix"));
+    }
+
+    public static String changeXY(String str) {
+        return "";
+    }
+}
