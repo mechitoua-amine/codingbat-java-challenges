b5b20b8b4a491778049331b9ea4769fac6d41e45
mehmet emin <mechitoua.m.amine@gmail.com>
2021-03-29 15:53:32 +0100

    adding Array220.java solution

--- src/code/codingbat/recursion/Array220.java
+++ src/code/codingbat/recursion/Array220.java
@@ -19,7 +19,9 @@ class Array220 {
         System.out.println(array220(new int[]{3, 30}, 0));
         System.out.println(array220(new int[]{3}, 0));
     }
-    public static boolean array220(int[], int index) {
-        return true;
+    public static boolean array220(int[] nums, int index) {
+        if (nums.length < 2 || index == nums.length-1) return false;
+        if (nums[index+1] == nums[index] * 10) return true;
+        return array220(nums, index + 1);
     }
 }
