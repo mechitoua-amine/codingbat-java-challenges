60aba1c97d8a5a7e0210da0e45a90a23cc82554a
mehmet emin <mechitoua.m.amine@gmail.com>
2021-03-30 10:26:42 +0100

    adding AllStar.java file

---
+++ src/code/codingbat/recursion/AllStar.java
@@ -0,0 +1,20 @@
+package code.codingbat.recursion;
+
+/**
+ * Given a string, compute recursively a new string where all the
+ * adjacent chars are now separated by a "*".
+ *
+ *  allStar("hello") → "h*e*l*l*o"
+ *  allStar("abc") → "a*b*c"
+ *  allStar("ab") → "a*b"
+ */
+
+class AllStar {
+    public static void main(String[] args) {
+
+    }
+
+    public static String allStar(String str){
+        return "";
+    }
+}
