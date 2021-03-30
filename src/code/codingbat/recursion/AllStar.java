a1497c26354679cdd44bb5dcd74326b12c1703a4
mehmet emin <mechitoua.m.amine@gmail.com>
2021-03-30 10:37:00 +0100

    adding AllStar.java solution

--- src/code/codingbat/recursion/AllStar.java
+++ src/code/codingbat/recursion/AllStar.java
@@ -11,10 +11,13 @@ package code.codingbat.recursion;
 
 class AllStar {
     public static void main(String[] args) {
-
+        System.out.println(allStar("hello"));
+        System.out.println(allStar("abc"));
+        System.out.println(allStar("ab"));
     }
 
     public static String allStar(String str){
-        return "";
+        if (str.length() < 2) return str;
+        return str.charAt(0)+ "*" + allStar(str.substring(1));
     }
 }
