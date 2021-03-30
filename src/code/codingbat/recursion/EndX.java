73a7a40d080f14ba60cfa02bb7e8fecb95ca8267
mehmet emin <mechitoua.m.amine@gmail.com>
2021-03-30 11:07:12 +0100

    adding EndX.java solution

--- src/code/codingbat/recursion/EndX.java
+++ src/code/codingbat/recursion/EndX.java
@@ -18,6 +18,9 @@ class EndX {
     }
 
     public static String endX(String str) {
-        return "";
+        if (str.length() == 0) return str;
+        if (str.charAt(0) == 'x')
+            return endX(str.substring(1)) + "x";
+        return str.charAt(0) + endX(str.substring(1));
     }
 }
