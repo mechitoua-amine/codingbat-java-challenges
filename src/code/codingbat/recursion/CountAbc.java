8a75b4f19ac1dcc69e6700b95ecadb49641542e5
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-03-31 11:10:29 +0100

    adding CountAbc.java solution

--- src/code/codingbat/recursion/CountAbc.java
+++ src/code/codingbat/recursion/CountAbc.java
@@ -17,6 +17,10 @@ class CountAbc {
     }
 
     public static int countAbc(String str) {
-        return 0;
+        if (str.length() < 3) return 0;
+        if (str.substring(0,3).equals("abc")
+            || str.substring(0,3).equals("aba"))
+            return 1 + countAbc(str.substring(1));
+        return countAbc(str.substring(1));
     }
 }
