577dfc0562eb500a2f9170d46fc76ab9a5dcb084
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-04 10:50:13 +0100

    adding StringClean.java solution

---
+++ src/code/codingbat/recursion/CountHi2.java
@@ -0,0 +1,22 @@
+package code.codingbat.recursion;
+
+/**
+ * Given a string, compute recursively the number of times lowercase "hi" appears in the string,
+ * however do not count "hi" that have an 'x' immedately before them.
+ *
+ * countHi2("ahixhi") → 1
+ * countHi2("ahibhi") → 2
+ * countHi2("xhixhi") → 0
+ */
+
+public class CountHi2 {
+    public static void main(String[] args) {
+        System.out.println(countHi2("ahixhi"));
+        System.out.println(countHi2("ahibhi"));
+        System.out.println(countHi2("xhixhi"));
+    }
+
+    public static int countHi2(String str) {
+        return 0;
+    }
+}
