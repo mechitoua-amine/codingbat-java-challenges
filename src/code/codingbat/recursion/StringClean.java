ae536f7417ec27d14ee81766e76f23bf68f065db
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-04 10:37:43 +0100

    adding StringClean.java file

---
+++ src/code/codingbat/recursion/StringClean.java
@@ -0,0 +1,23 @@
+package code.codingbat.recursion;
+
+/**
+ * Given a string, return recursively a "cleaned" string where adjacent chars
+ * that are the same have been reduced to a single char. So "yyzzza" yields "yza".
+ *
+ *
+ * stringClean("yyzzza") → "yza"
+ * stringClean("abbbcdd") → "abcd"
+ * stringClean("Hello") → "Helo"
+ */
+
+public class StringClean {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    public static String stringClean(String str) {
+        return "";
+    }
+}
