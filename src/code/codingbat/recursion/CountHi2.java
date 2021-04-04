9ab1b5c90e361cf71252abe5049a21b44c5debca
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-04 11:01:30 +0100

    adding CountHi2.java solution

--- src/code/codingbat/recursion/CountHi2.java
+++ src/code/codingbat/recursion/CountHi2.java
@@ -17,6 +17,14 @@ public class CountHi2 {
     }
 
     public static int countHi2(String str) {
-        return 1;
+        if (str.length() < 2) return 0;
+        if (str.startsWith("hi"))
+            return 1 + countHi2(str.substring(2));
+        if (str.charAt(0) == 'x' && str.length() >= 3) {
+            if (str.startsWith("hi", 1))
+                return countHi2(str.substring(3));
+            return countHi2(str.substring(1));
+        }
+        return countHi2(str.substring(1));
     }
 }
