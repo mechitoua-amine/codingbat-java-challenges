61f8361294b3cdd797707c983e46e7be6ce8b176
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-04 10:34:03 +0100

    adding Count11.java solution

--- src/code/codingbat/recursion/Count11.java
+++ src/code/codingbat/recursion/Count11.java
@@ -17,6 +17,8 @@ public class Count11 {
     }
 
     public static int count11(String str) {
-        return 0;
+        if (str.length() < 2) return 0;
+        if (str.substring(0,2).equals("11")) return 1 + count11(str.substring(2));
+        return count11(str.substring(1));
     }
 }
