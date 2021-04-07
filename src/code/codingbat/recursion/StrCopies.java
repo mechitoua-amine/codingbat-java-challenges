95f66f69c71948b43da4a98697d4ff02fa4b17f4
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-07 10:53:33 +0100

    adding StrCopies.java solution

--- src/code/codingbat/recursion/StrCopies.java
+++ src/code/codingbat/recursion/StrCopies.java
@@ -7,16 +7,34 @@ package code.codingbat.recursion;
  *
  * strCopies("catcowcat", "cat", 2) → true
  * strCopies("catcowcat", "cow", 2) → false
- * strcopies("catcowcat", "cow", 1) → true
+ * strCopies("catcowcat", "cow", 1) → true
  */
 class StrCopies {
     public static void main(String[] args) {
         System.out.println(strCopies("catcowcat", "cat", 2));
         System.out.println(strCopies("catcowcat", "cow", 2));
-        System.out.println(strcopies("catcowcat", "cow", 1));
+        System.out.println(strCopies("catcowcat", "cow", 1));
     }
 
     public static boolean strCopies(String str, String sub, int n) {
-        return true;
+        if (str.length() < sub.length()) return (n <= 0);
+        if (str.substring(0, sub.length()).equals(sub))
+            return strCopies(str.substring(1), sub, n - 1);
+        return strCopies(str.substring(1), sub, n);
     }
+
+    /**
+     *  public boolean strCopies(String str, String sub, int n) {
+            if (n == 0) return true;
+            int len = sub.length();
+            if (str.length() < len) return false;
+
+            if (str.substring(0, len).equals(sub)) {
+                // Found it, so subtract 1 from n in the recursion
+                return strCopies(str.substring(1), sub, n-1);
+            } else {
+                return strCopies(str.substring(1), sub, n);
+            }
+        }
+     */
 }
