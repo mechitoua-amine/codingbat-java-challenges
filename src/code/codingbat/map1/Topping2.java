edf773843bcd0bebdcc85d37f4d67aa6a2238a7a
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-18 20:23:28 +0100

    adding Topping2.java file

---
+++ src/code/codingbat/map1/Topping2.java
@@ -0,0 +1,37 @@
+package code.codingbat.map1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given a map of food keys and their topping values, modify and return
+ * the map as follows: if the key "ice cream" has a value, set that as
+ * the value for the key "yogurt" also. If the key "spinach" has a value,
+ * change that value to "nuts".
+ *
+ * topping2({"ice cream": "cherry"}) → {"yogurt": "cherry", "ice cream": "cherry"}
+ * topping2({"spinach": "dirt", "ice cream": "cherry"})
+ *      → {"yogurt": "cherry", "spinach": "nuts", "ice cream": "cherry"}
+ * topping2({"yogurt": "salt"}) → {"yogurt": "salt"}
+ */
+
+public class Topping2 {
+    public static void main(String[] args) {
+        System.out.println(topping2(new HashMap<>(){
+            {put("ice cream", "cherry");}
+        }));
+        System.out.println(topping2(new HashMap<>(){
+            {
+                put("spinach", "dirt");
+                put("ice cream", "cherry");
+            }
+        }));
+        System.out.println(topping2(new HashMap<>(){
+            {put("yogurt", "salt");}
+        }));
+    }
+
+    public static Map<String, String> topping2(Map<String, String> map) {
+        return new HashMap<>();
+    }
+}
