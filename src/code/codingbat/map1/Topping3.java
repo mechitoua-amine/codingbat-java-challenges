927f95cc441d4c11fcb586063c46e22a735774f8
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-18 21:54:48 +0100

    adding Topping3.java file

---
+++ src/code/codingbat/map1/Topping3.java
@@ -0,0 +1,42 @@
+package code.codingbat.map1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given a map of food keys and topping values, modify and return
+ * the map as follows: if the key "potato" has a value, set that
+ * as the value for the key "fries". If the key "salad" has a value,
+ * set that as the value for the key "spinach".
+ *
+ *
+ * topping3({"potato": "ketchup"}) → {"potato": "ketchup", "fries": "ketchup"}
+ * topping3({"potato": "butter"}) → {"potato": "butter", "fries": "butter"}
+ * topping3({"salad": "oil", "potato": "ketchup"})
+ *      → {"spinach": "oil", "salad": "oil", "potato": "ketchup", "fries": "ketchup"}
+ */
+
+public class Topping3 {
+    public static void main(String[] args) {
+        System.out.println(topping3(new HashMap<>() {
+            {
+                put("potato", "ketchup");
+            }
+        }));
+        System.out.println(topping3(new HashMap<>() {
+            {
+                put("potato", "butter");
+            }
+        }));
+        System.out.println(topping3(new HashMap<>() {
+            {
+                put("salad", "oil");
+                put("potato", "ketchup");
+            }
+        }));
+    }
+
+    public static Map<String, String> topping3(Map<String, String> map) {
+        return new HashMap<>();
+    }
+}
