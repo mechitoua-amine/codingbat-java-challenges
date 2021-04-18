e442c7825a15994c71994e863b2398b70c9df5ea
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-18 19:08:28 +0100

    adding MapShare.java file

---
+++ src/code/codingbat/map1/MapShare.java
@@ -0,0 +1,41 @@
+package code.codingbat.map1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Modify and return the given map as follows: if the key "a" has a value, set the key "b"
+ * to have that same value. In all cases remove the key "c", leaving the rest of the map unchanged.
+ *
+ * mapShare({"a": "aaa", "b": "bbb", "c": "ccc"}) → {"a": "aaa", "b": "aaa"}
+ * mapShare({"b": "xyz", "c": "ccc"}) → {"b": "xyz"}
+ * mapShare({"a": "aaa", "c": "meh", "d": "hi"}) → {"a": "aaa", "b": "aaa", "d": "hi"}
+ */
+public class MapShare {
+    public static void main(String[] args) {
+        System.out.println(mapShare(new HashMap<>() {
+            {
+                put("a", "aaa");
+                put("b", "bbb");
+                put("c", "ccc");
+            }
+        }));
+        System.out.println(mapShare(new HashMap<>() {
+            {
+                put("b", "xyz");
+                put("c", "ccc");
+            }
+        }));
+        System.out.println(mapShare(new HashMap<>() {
+            {
+                put("a", "aaa");
+                put("c", "meh");
+                put("d", "hi");
+            }
+        }));
+    }
+
+    public static Map<String, String> mapShare(Map<String, String> map) {
+        return new HashMap<>();
+    }
+}
