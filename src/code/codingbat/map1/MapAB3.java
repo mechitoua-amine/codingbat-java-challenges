6a72ffd2fd20527594e4ffbf09cb0aeff4399209
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-19 09:55:40 +0100

    adding MapAB3.java solution

--- src/code/codingbat/map1/MapAB3.java
+++ src/code/codingbat/map1/MapAB3.java
@@ -38,6 +38,10 @@ public class MapAB3 {
     }
 
     public static Map<String, String> mapAB3(Map<String, String> map) {
-        return new HashMap<>();
+        if (map.containsKey("a") && !map.containsKey("b"))
+            map.put("b", map.get("a"));
+        if (map.containsKey("b") && !map.containsKey("a"))
+            map.put("a", map.get("b"));
+        return map;
     }
 }
