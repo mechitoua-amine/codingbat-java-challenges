b4568f938a89201dae4e12b99a98fd082fd49524
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-19 10:59:19 +0100

    adding MapAB4.java solution

--- src/code/codingbat/map1/MapAB4.java
+++ src/code/codingbat/map1/MapAB4.java
@@ -39,6 +39,16 @@ public class MapAB4 {
     }
 
     public static Map<String, String> mapAB4(Map<String, String> map) {
-        return new HashMap<>();
+        if (map.containsKey("a") && map.containsKey("b")) {
+            String a = map.get("a");
+            String b = map.get("b");
+            if (a.length() > b.length()) map.put("c", a);
+            else if (b.length() > a.length()) map.put("c", b);
+            else {
+                map.put("a", "");
+                map.put("b", "");
+            }
+        }
+        return map;
     }
 }
