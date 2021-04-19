6567141ba33db4681131263e287d6e6391feb367
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-19 09:32:03 +0100

    adding MapAB3.java file

---
+++ src/code/codingbat/map1/MapAB3.java
@@ -0,0 +1,43 @@
+package code.codingbat.map1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Modify and return the given map as follows: if exactly one
+ * of the keys "a" or "b" has a value in the map (but not both),
+ * set the other to have that same value in the map.
+ *
+ *
+ * mapAB3({"a": "aaa", "c": "cake"}) → {"a": "aaa", "b": "aaa", "c": "cake"}
+ * mapAB3({"b": "bbb", "c": "cake"}) → {"a": "bbb", "b": "bbb", "c": "cake"}
+ * mapAB3({"a": "aaa", "b": "bbb", "c": "cake"}) → {"a": "aaa", "b": "bbb", "c": "cake"}
+ */
+
+public class MapAB3 {
+    public static void main(String[] args) {
+        System.out.println(mapAB3(new HashMap<>(){
+            {
+                put("a", "aaa");
+                put("c", "cake");
+            }
+        }));
+        System.out.println(mapAB3(new HashMap<>(){
+            {
+                put("b", "bbb");
+                put("c", "cake");
+            }
+        }));
+        System.out.println(mapAB3(new HashMap<>(){
+            {
+                put("a", "aaa");
+                put("b", "bbb");
+                put("c", "cake");
+            }
+        }));
+    }
+
+    public static Map<String, String> mapAB3(Map<String, String> map) {
+        return new HashMap<>();
+    }
+}
