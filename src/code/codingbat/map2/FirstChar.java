95e1a0ab9abee20f9d2d54c35a153a8c1d86a11a
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-21 11:57:42 +0100

    adding FirstChar.jav file

---
+++ src/code/codingbat/map2/FirstChar.java
@@ -0,0 +1,29 @@
+package code.codingbat.map2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of non-empty strings, return a Map<String, String>
+ * with a key for every different first character seen, with the
+ * value of all the strings starting with that character appended
+ * together in the order they appear in the array.
+ *
+ * firstChar(["salt", "tea", "soda", "toast"])
+ *      → {"s": "saltsoda", "t": "teatoast"}
+ * firstChar(["aa", "bb", "cc", "aAA", "cCC", "d"])
+ *      → {"a": "aaaAA", "b": "bb", "c": "cccCC", "d": "d"}
+ * firstChar([]) → {}
+ */
+
+public class FirstChar {
+    public static void main(String[] args) {
+        System.out.println(firstChar(new String[] {"salt", "tea", "soda", "toast"}));
+        System.out.println(firstChar((new String[] {"aa", "bb", "cc", "aAA", "cCC", "d"})));
+        System.out.println(firstChar((new String[] {})));
+    }
+
+    public static Map<String, String> firstChar(String[] strings) {
+        return new HashMap<>();
+    }
+}
