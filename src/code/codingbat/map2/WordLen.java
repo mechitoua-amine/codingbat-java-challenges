af22c66ed7cfdf78cc6a1d431a3fd4a86af92912
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-21 11:17:39 +0100

    adding WordLen.java solution

--- src/code/codingbat/map2/WordLen.java
+++ src/code/codingbat/map2/WordLen.java
@@ -21,6 +21,11 @@ public class WordLen {
     }
 
     public static Map<String, Integer> wordLen(String[] strings) {
-        return new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (String str : strings) {
+           map.put(str, str.length());
+        }
+
+        return map;
     }
 }
