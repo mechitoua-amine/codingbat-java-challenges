d0276aa20bb72ba387e7ea5ca567f521d54ce64c
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-04-26 12:15:45 +0100

    adding GroupSum5.java file

---
+++ src/code/codingbat/recursion2/GroupSum5.java
@@ -0,0 +1,29 @@
+package code.codingbat.recursion2;
+
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+/**
+ * Given an array of ints, is it possible to choose a group of some of the ints,
+ * such that the group sums to the given target with these additional
+ * constraints: all multiples of 5 in the array must be included in the group.
+ * If the value immediately following a multiple of 5 is 1, it must not be
+ * chosen. (No loops needed.)
+ *
+ * groupSum5(0, [2, 5, 10, 4], 19) → true
+ *
+ * groupSum5(0, [2, 5, 10, 4], 17) → true
+ *
+ * groupSum5(0, [2, 5, 10, 4], 12) → false
+ */
+
+public class GroupSum5 {
+    public static void main(String[] args) {
+        System.out.println(groupSum5(0, new int[] { 2, 5, 10, 4 }, 19));
+        System.out.println(groupSum5(0, new int[] { 2, 5, 10, 4 }, 17));
+        System.out.println(groupSum5(0, new int[] { 2, 5, 10, 4 }, 12));
+    }
+
+    public static Boolean groupSum5(int start, int[] nums, int target) {
+        return true;
+    }
+}
