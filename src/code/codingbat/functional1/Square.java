f6459ba2ac898084b7c3bbffe7e0e54e6df59c11
mechitoua mohammed amine <mechitoua_amine@protonmail.ch>
2021-05-01 13:36:38 +0100

    adding Square.java file

---
+++ src/code/codingbat/functional1/Square.java
@@ -0,0 +1,25 @@
+package code.codingbat.functional1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given a list of integers, return a list where each integer is multiplied with itself.
+ *
+ * square([1, 2, 3]) → [1, 4, 9]
+ * square([6, 8, -6, -8, 1]) → [36, 64, 36, 64, 1]
+ * square([]) → []
+ */
+
+public class Square {
+    public static void main(String[] args) {
+        System.out.println(square(new ArrayList<>(Arrays.asList(1,2,3))));
+        System.out.println(square(new ArrayList<>(Arrays.asList(6, 8, -6, -8, 1))));
+        System.out.println(square(new ArrayList<>()));
+    }
+
+    public static List<Integer> square(List<Integer> nums) {
+        return new ArrayList<>();
+    }
+}
