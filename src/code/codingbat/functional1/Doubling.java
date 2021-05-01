e93587f6c360dae60824f0e9abd558dc309b4c31
mechitoua mohammed amine <mechitoua_amine@protonmail.ch>
2021-05-01 13:27:41 +0100

    adding Doubling.java solution

--- src/code/codingbat/functional1/Doubling.java
+++ src/code/codingbat/functional1/Doubling.java
@@ -1,5 +1,9 @@
 package code.codingbat.functional1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Given a list of integers, return a list where each integer is multiplied by
  * 2.
@@ -13,12 +17,18 @@ package code.codingbat.functional1;
 
 public class Doubling {
     public static void main(String[] args) {
-        System.out.println(doubling(1, 2, 3));
-        System.out.println(doubling(6, 8, 6, 8, -1));
-        System.out.println(doubling());
+        System.out.println(doubling(new ArrayList<>(Arrays.asList(1, 2, 3))));
+        System.out.println(doubling(new ArrayList<>(Arrays.asList(6, 8, 6, 8, -1))));
+        System.out.println(doubling(new ArrayList<>()));
     }
 
-    public static int[] doubling(int... nums) {
-        return new int[] {};
+    public static List<Integer> doubling(List<Integer> nums) {
+        nums.replaceAll(n -> n * 2);
+        return nums;
+
+        // OR the equivalent java streams solution:
+        // return nums.stream()
+        // .map(n -> n * 2)
+        // .collect(Collectors.toList());
     }
 }
