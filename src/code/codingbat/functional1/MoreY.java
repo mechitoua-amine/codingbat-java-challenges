882d3a6d4a395fd055f39535e3153d7d8ff464e8
mechitoua mohammed amine <mechitoua_amine@protonmail.ch>
2021-05-02 10:14:57 +0100

    adding MoreY.java file

---
+++ src/code/codingbat/functional1/MoreY.java
@@ -0,0 +1,28 @@
+package code.codingbat.functional1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given a list of strings, return a list where each
+ * string has "y" added at its start and end.
+ *
+ *
+ * moreY(["a", "b", "c"]) → ["yay", "yby", "ycy"]
+ * moreY(["hello", "there"]) → ["yhelloy", "ytherey"]
+ * moreY(["yay"]) → ["yyayy"]
+ */
+
+public class MoreY {
+    public static void main(String[] args) {
+        System.out.println(moreY(new ArrayList<>(Arrays.asList("a", "b", "c"))));
+        System.out.println(moreY(new ArrayList<>(Arrays.asList("hello", "there"))));
+        System.out.println(moreY(new ArrayList<>(Collections.singletonList("yay"))));
+    }
+
+    public static List<String> moreY(List<String> strings) {
+        return new ArrayList<>();
+    }
+}
