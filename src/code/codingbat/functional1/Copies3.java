f2b2fdb2a62b3f34e3d7e18219ffaba1682160b4
mechitoua mohammed amine <mechitoua_amine@protonmail.ch>
2021-05-02 10:06:40 +0100

    adding Copies.java file

---
+++ src/code/codingbat/functional1/Copies3.java
@@ -0,0 +1,26 @@
+package code.codingbat.functional1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given a list of strings, return a list where each string is replaced
+ * by 3 copies of the string concatenated together.
+ *
+ * copies3(["a", "bb", "ccc"]) → ["aaa", "bbbbbb", "ccccccccc"]
+ * copies3(["24", "a", ""]) → ["242424", "aaa", ""]
+ * copies3(["hello", "there"]) → ["hellohellohello", "theretherethere"]
+ */
+
+public class Copies3 {
+    public static void main(String[] args) {
+        System.out.println(copies3(new ArrayList<>(Arrays.asList("a", "bb", "ccc"))));
+        System.out.println(copies3(new ArrayList<>(Arrays.asList("24", "a", ""))));
+        System.out.println(copies3(new ArrayList<>(Arrays.asList("hello", "there"))));
+    }
+
+    public static List<String> copies3(List<String> strings) {
+        return new ArrayList<>();
+    }
+}
