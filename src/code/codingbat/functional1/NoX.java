170bbb4675d1fb584e131f4fcdc91b9c1391c1f0
mechitoua mohammed amine <mechitoua_amine@protonmail.ch>
2021-05-03 17:36:29 +0100

    adding NoX.java file

---
+++ src/code/codingbat/functional1/NoX.java
@@ -0,0 +1,26 @@
+package code.codingbat.functional1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given a list of strings, return a list where each string has all its "x" removed.
+ *
+ * noX(["ax", "bb", "cx"]) → ["a", "bb", "c"]
+ * noX(["xxax", "xbxbx", "xxcx"]) → ["a", "bb", "c"]
+ * noX(["x"]) → [""]
+ */
+
+public class NoX {
+    public static void main(String[] args) {
+        System.out.println(noX(new ArrayList<>(Arrays.asList("ax", "bb", "cx"))));
+        System.out.println(noX(new ArrayList<>(Arrays.asList("xxax", "xbxbx", "xxcx"))));
+        System.out.println(noX(new ArrayList<>(Collections.singletonList("x"))));
+    }
+
+    public static List<String> noX(List<String> strings) {
+        return new ArrayList<>();
+    }
+}
