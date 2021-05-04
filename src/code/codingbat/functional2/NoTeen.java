854649e0d22b60d0535937f5a773d3f345de6977
mechitoua mohammed amine <mechitoua_amine@protonmail.ch>
2021-05-04 18:36:29 +0100

    adding NoTeen.java file

---
+++ src/code/codingbat/functional2/NoTeen.java
@@ -0,0 +1,27 @@
+package code.codingbat.functional2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given a list of integers, return a list of those numbers,
+ * omitting any that are between 13 and 19 inclusive.
+ *
+ * noTeen([12, 13, 19, 20]) → [12, 20]
+ * noTeen([1, 14, 1]) → [1, 1]
+ * noTeen([15]) → []
+ */
+
+public class NoTeen {
+    public static void main(String[] args) {
+        System.out.println(noTeen(new ArrayList<>(Arrays.asList(12, 13, 19, 20))));
+        System.out.println(noTeen(new ArrayList<>(Arrays.asList(1, 14, 1))));
+        System.out.println(noTeen(new ArrayList<>(Collections.singletonList(15))));
+    }
+
+    public static List<Integer> noTeen(List<Integer> nums) {
+        return new ArrayList<>();
+    }
+}
